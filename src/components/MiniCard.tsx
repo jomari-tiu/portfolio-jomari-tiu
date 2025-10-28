d2577d22ebf237a059a1f55a621262da95bf6cc9
Jomari Tiu <jomtiu16@gmail.com>
2025-10-28 23:21:48 +0800

    :construction_worker: fix error

--- src/components/MiniCard.tsx
+++ src/components/MiniCard.tsx
@@ -22,11 +22,9 @@ const MiniCard = ({ data, priority = false }: Props) => {
           />
         </figure>
         <div className="p-5">
-          <a href="#">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-accent">
-              {data.title}
-            </h5>
-          </a>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-accent">
+            {data.title}
+          </h5>
           {data.organization && (
             <div className="mb-3 font-normal text-gray-400">
               {data.organization}
